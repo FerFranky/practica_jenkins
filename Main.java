9038b0ba2729163fd31825511d051d3ceba763d2
FERNANDO OLMOS <fernando.olmos@afirma-solutions.com>
2023-11-15 19:19:40 -0600

    Feat: Test jenkins compilacion

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ class Main {
     public static void main(String[] args){
         System.out.println("Hola mundo");
         System.out.println("Prueba Jenkins");
+        System.out.println("Test compilacion");
     }
 }
